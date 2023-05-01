6c784be7481fa20d04858031606d6c37eb0155cb
Mohamad anas fattoum <samaanas30@gmail.com>
2023-05-01 22:29:36 +0200

    js comprsions

--- code.js
+++ code.js
@@ -1,10 +1,31 @@
-let ismarried = confirm('are you married ? ')
+// let ismarried = confirm('are you married ? ')
 
-console.log(ismarried)
+// console.log(ismarried)
 
 
-alert('hello mystro')
+// alert('hello mystro')
 
-let age = prompt('Enter Your Age : ',20)
+// let age = prompt('Enter Your Age : ',20)
 
-console.log(Age)
+// console.log(Age)
+
+
+// alert(1==2); /f
+
+// alert(1<2); /t
+// alert(1>2); /f
+
+// let x = (5<4)
+
+// console.log(x)
+
+
+// let y = (5>4)##
+
+// console.log(y)
+
+
+let year = prompt ('how old you ?' ,'21')
+if (year=20) alert ('you are right!')
+
+console.log(year=20)
